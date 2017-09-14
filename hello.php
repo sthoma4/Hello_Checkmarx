da4075d12f740c194a582209225b9f413907a6c3
sthoma4 <stuart.thomas@hallmark.com>
2017-09-14 11:38:11 -0500

    initial load

---
+++ hello.php
@@ -0,0 +1,32 @@
+?php
+
+// examples of $_SERVER variables that are considered NOT SAFE and needs sanitizing
+//$domain = $_SERVER['HTTP_HOST'];
+//if (isset($domain)) {
+//    //$domain_sanitized = htmlspecialchars($domain, ENT_QUOTES | ENT_HTML401, 'UTF-8');
+//    $domain_sanitized = filter_var($domain, FILTER_SANITIZE_SPECIAL_CHARS,
+//        array('flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_ENCODE_HIGH));
+//}
+
+if (isset($_SERVER['HTTP_HOST'])) {
+    $_SERVER['HTTP_HOST'] = filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_SPECIAL_CHARS,
+        array('flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_ENCODE_HIGH));
+}
+
+//if (isset($_SERVER['REQUEST_URI'])) {
+//    $_SERVER['REQUEST_URI'] = htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES | ENT_HTML401, 'UTF-8');
+//}
+
+//$request_uri = $_SERVER['REQUEST_URI'];
+
+?>
+
+<html>
+<head>`
+  <title>PHP Test</title>
+</head>
+<body>
+
+<?php echo '<p>'.$_SERVER['HTTP_HOST'].'</p>'; ?>
+</body>
+</html>
